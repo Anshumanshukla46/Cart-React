154d94e332a23a3521c0e6e367fb7ea2a320eb16
Anshuman Shukla <Anshumanshukla46@gmail.com>
2022-09-23 23:02:09 +0530

    Adding images of action buttons

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,8 +2,23 @@ import React from "react";
 
 class CartItem extends React.Component {
 
+    // State
+    constructor() {
+        super(); // calling constructor of super class
+        this.state = {
+            price: 999,
+            title: "Mobile Phone",
+            qty: 1,
+            img: ""
+        }
+    }
+
     // to convert class component to react component
     render() {
+
+        // object structuring
+        const { price, title, qty } = this.state;
+
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -16,11 +31,16 @@ class CartItem extends React.Component {
 
                     {/* can add object directly */}
 
-                    <div style={{ fontSize: 25 }}>Phone</div>
-                    <div style={{ color: "gray" }}>Rs. 999</div>
-                    <div style={{ color: "gray" }}>Qty: 1</div>
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: "gray" }}>Rs {price}</div>
+                    <div style={{ color: "gray" }}>Qty: {qty}</div>
                     <div className="cart-item-actions">
-                        {/*  */}
+                        {/* Buttons */}
+
+                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/748/748113.png" />
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
+                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png" />
+
                     </div>
 
                 </div>
