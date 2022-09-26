b08f3bbc0a7469405cf16a9ca535d54221de6dfd
Anshuman Shukla <Anshumanshukla46@gmail.com>
2022-09-26 19:56:52 +0530

    Extended version

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Cart from './Cart';
 
 import Navbar from './Navbar';
 
-import * as firebase from 'firebase';
+// for using firebase
+import { firestore as db } from './firebase-config'
+import firebase from './firebase-config';
+
 
 // Root component
 class App extends React.Component {
@@ -14,8 +17,10 @@ class App extends React.Component {
   // State
   constructor() {
     super(); // calling constructor of super class  (React.Component)
+
     this.state = {
-      products: []
+      products: [],
+      loading: true
     }
 
     // this.increaseQuantity = this.increaseQuantity.bind(this);
@@ -23,23 +28,181 @@ class App extends React.Component {
 
   }
 
+
+  componentDidMount() {
+
+    /*
+    firebase
+      .firestore()
+      .collection('product')
+      .get()
+      .then((snapshot) => {
+//      then -> means it is returning me some promise
+
+        console.log(snapshot);
+        // currently if we do any change in firebase then we have toi refresh but we do not want to refresh then
+        // we have to use 'eventListener()' and 'onSnapshot()in place of get'
+
+
+        snapshot.docs.map((doc) => {
+          console.log(doc.data());
+        });
+
+
+
+
+        const products = snapshot.docs.map((doc) => {
+
+          const data = doc.data();
+          // adding the auto generated id as well as otherwise it gives the warning of not 'having a unique id'
+          data['id'] = doc.id;
+
+
+          return data;
+        });
+
+
+        this.setState({
+          products,
+          loading: false
+        });
+
+      });
+    */
+
+
+    // now without refreshing the data can be fetched 
+    // firebase
+    //   .firestore()
+    //   .collection('product')
+    //   .onSnapshot((snapshot) => {
+
+    //     // all this happen due to onSnapshot;
+
+    //     console.log(snapshot);
+
+    //     snapshot.docs.map((doc) => {
+    //       console.log(doc.data())
+    //     });
+
+    //     const products = snapshot.docs.map((doc) => {
+
+    //       const value = doc.data();
+
+    //       value['id'] = doc.id;
+    //       return value;
+    //     });
+
+    //     this.setState({
+    //       products,
+    //       loading: false
+    //     });
+    //   })
+
+
+    // Quering the product using the "where(_,_,_)" before "onSnapshot"
+
+    //  can have more the one where 
+
+
+    firebase
+      .firestore()
+      .collection('product')
+      // .where('price', '==', 99)
+      // .where('title', '==', 'Mouse')
+      // .orderBy('price', 'asc')
+      .orderBy('price', 'desc')
+      .onSnapshot((snapshot) => {
+
+        // all this happen due to onSnapshot;
+
+        console.log(snapshot);
+
+        snapshot.docs.map((doc) => {
+          console.log(doc.data())
+        });
+
+        const products = snapshot.docs.map((doc) => {
+
+          const value = doc.data();
+
+          value['id'] = doc.id;
+          return value;
+        });
+
+        this.setState({
+          products,
+          loading: false
+        });
+      })
+
+  }
+
+
+
+  // added data into the firebase
+  // arrow function
+  addProduct = () => {
+    firebase
+      .firestore()
+      .collection('product')
+      .add({
+        img: '',
+        price: 900,
+        qty: 3,
+        title: 'washing machine'
+      })
+      .then((docRef) => {
+        console.log("added ", docRef);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+
+  }
+
+
+
   handleIncreaseQuantity = (product) => {
     console.log("increase the qty", product);
 
     const { products } = this.state;
     const index = products.indexOf(product);
 
-    products[index].qty += 1;
 
-    this.setState({
-      products: products // products -> short-hand
-    })
+    // commented just to get the quantity now from the database not by 'eventListener'
+
+    /*
+        products[index].qty += 1;
+    
+        this.setState({
+          products: products // products -> short-hand
+        })
+    */
+
+
+    // using this we will get the docRef and then update accordingly
+    const docRef = firebase.firestore().collection('product').doc(products[index].id);
+
+
+    docRef
+      .update({
+        qty: products[index].qty + 1
+      })
+      .then(() => {
+        console.log('updated successfully');
+      })
+      .catch((error) => {
+        console.log("Error : ", error);
+      })
 
   }
 
   // product as argument
   handleDecreaseQuantity = (product) => {
     console.log('dec the qty of ', product);
+
     const { products } = this.state;
     const index = products.indexOf(product);
 
@@ -47,23 +210,59 @@ class App extends React.Component {
       return;
     }
 
-    products[index].qty -= 1;
+    /*  
+        products[index].qty -= 1;
+    
+        this.setState({
+          products: products
+        })
+    
+    */
 
-    this.setState({
-      products
-    })
+
+    const docRef = firebase.firestore().collection('product').doc(products[index].id);
+
+    docRef
+      .update({
+        qty: products[index].qty - 1
+      })
+      .then(() => {
+        console.log('updated successfully');
+      })
+      .catch((error) => {
+        console.log("Error : ", error);
+      })
   }
+
+
 
   // id as argument
   handleDeleteProduct = (id) => {
     const { products } = this.state;
 
-    const items = products.filter((item) => item.id !== id); // [{array}]
+    /*    const items = products.filter((item) => item.id !== id); // [{array}]
+    
+        this.setState({
+          products: items
+        })
+    */
 
-    this.setState({
-      products: items
-    })
+    // same as update
+    const docRef = firebase.firestore().collection('product').doc(id);
+
+    docRef
+      .delete()
+      .then(() => {
+        console.log('Deleted successfully');
+      })
+      .catch((error) => {
+        console.log("Error : ", error);
+      })
+
+
   }
+
+
 
   getCartCount = () => {
     const { products } = this.state;
@@ -78,6 +277,7 @@ class App extends React.Component {
   }
 
 
+
   getCartTotal = () => {
     const { products } = this.state;
 
@@ -90,13 +290,17 @@ class App extends React.Component {
     return cartTotal;
   }
 
+
   render() {
-    const { products } = this.state;
+    const { products, loading } = this.state;
     return (
       <div className="App">
 
         {/* using after importing  */}
         <Navbar count={this.getCartCount()} />
+
+        {/* <button onClick={this.addProduct} style={{ padding: 20, fontSize: 20 }}> Add a product </button> */}
+
 
         {/* passing via props */}
         <Cart
@@ -109,7 +313,9 @@ class App extends React.Component {
           onDeleteProduct={this.handleDeleteProduct}
         />
 
-        <div style={{ fontSize: 20, padding: 20 }}>TOTAL: {this.getCartTotal()}</div>
+        {loading && <h1>Loading Products ... </h1>}
+
+        <div style={{ fontSize: 20, padding: 10 }}>TOTAL: {this.getCartTotal()}</div>
 
       </div>
     );
@@ -117,3 +323,4 @@ class App extends React.Component {
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -4,30 +4,6 @@ import './index.css';
 import App from './App';
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAWCgWLRDYk7t1SSf93Y_cSwauojg43SFU",
-  authDomain: "cart-eadfc.firebaseapp.com",
-  projectId: "cart-eadfc",
-  storageBucket: "cart-eadfc.appspot.com",
-  messagingSenderId: "195440508131",
-  appId: "1:195440508131:web:f31fa57b0ea070920f99d6",
-  measurementId: "G-1L0W9ZGJMY"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -35,4 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
